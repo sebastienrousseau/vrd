cfe9b451f2054624cfe36b9da37f81d3f8bb3a50
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-05-10 23:22:03 +0100

    test(vrd): updated test_main

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,11 +1,13 @@
 #[cfg(test)]
 mod tests {
+    use std::process;
     use vrd::run;
 
     #[test]
     fn test_run() {
         if let Err(err) = run() {
             eprintln!("Error running vrd: {}", err);
+            process::exit(1);
         }
         assert_eq!(1, 1)
     }
